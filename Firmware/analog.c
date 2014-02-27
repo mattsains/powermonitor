ab53ccde041942afaf0af8dd95ec5b2aa6339251
Matthew Sainsbury <matthew@sainsbury.za.net>
2014-02-27 18:52:02 +0200

    Initial commit - some firmware stuff

---
+++ Firmware/analog.c
@@ -0,0 +1,12 @@
+//This code handles the measuring of the two/three analog inputs we have
+
+
+//This is an Interrupt Service Routine (ISR)
+// When an interrupt is triggered, the code is paused, and then this code is run.
+// This code must be pretty fast to avoid interrupts stacking up
+// This interrupt handles the interrupt that gets triggered when the microcontroller has
+// finished measuring an analog input
+ISR(ADC_vect)
+{
+   
+}
---
+++ Firmware/main.c
@@ -0,0 +1,11 @@
+#include <avr/io.h>   //defines the registers we can access
+#include <stdint.h>   //defines some data types like int and long
+#include <avr/interrupt.h> //lets us use interrupts
+
+#include "analog.c"
+
+// When the microcontroller boots, this code runs
+int main()
+{
+   
+}
