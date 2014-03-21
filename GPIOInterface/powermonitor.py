9ed82a9a34c4f24eb0b82d28d7a5b35750581cf0
Matthew Sainsbury <matthew@sainsbury.za.net>
2014-03-21 16:40:39 +0000

    Updated the python API with today's firmware changes

--- GPIOInterface/powermonitor.py
+++ GPIOInterface/powermonitor.py
@@ -33,3 +33,46 @@ class PowerMonitor:
             }
         else:
             return False
+
+    """Returns two numbers representing the raw measurements from the current and voltage sensor"""
+    def read_raw(self):
+        response=self._send_data(0b001, 4)
+        return { "CURRENT": response[0]<<8|response[1],
+                 "VOLTAGE": response[2]<<8|response[3]
+                 }
+
+    """Returns the watts being used right now"""
+    def read_watts(self):
+        response=self._send_data(0b010, 2)
+        return response[0]<<8|response[1]
+
+    """Returns the measured power factor of the device (between -1 and 1)"""
+    def read_power_factor(self):
+        return self._send_data(0b011, 1) - 128
+
+    """Returns a map containing the calibration paramaters. See specification for details"""
+    """If something goes wrong, will throw an exception"""
+    def read_calibration(self):
+        response=self._send_data(0b100, 7)
+
+        sum=0
+        for (byte in response[:-1]):
+            sum+=byte
+            
+        if (sum!=response[-1]):
+            raise Exception("IO Error", "The calibration parameter checksum was incorrect")
+
+        return {
+            "FILTER_STRENGTH": response[0],
+            "SIGNAL_OFFSET": response[1]<<8|response[2],
+            "CURRENT_SCALE": response[3],
+            "VOLTAGE_SCALE": response[4],
+            "VOLTAGE_PHASE_DELAY": response[5]
+            }
+
+    """Writes calibration parameters to the microcontroller"""
+    def write_calibration(self, filter_strength, signal_offset, current_scale, voltage_scale, voltage_phase_delay):
+        sum=(filter_strength+signal_offset+current_scale+voltage_scale+voltage_phase_delay)%256
+        response=self._send_data(0b101, 1, filter_strength, signal_offset, current_scale, voltage_scale, voltage_phase_delay, sum)
+        if (response!=0b10101010):
+            raise Exception("IO Error", "Did not receive calibration write confirmation from the microcontroller - did it actually write?")
