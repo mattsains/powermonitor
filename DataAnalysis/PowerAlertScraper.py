e9a4fa2621b7fbf5c9d5e7d469eefdaaac9ca190
Kevin Beyers <kevinjamesbeyers@gmail.com>
2014-06-13 10:48:26 +0200

    - Fixed some python2 issues that were overlooked previously - Created database setup script...play around with it

--- DataAnalysis/PowerAlertScraper.py
+++ DataAnalysis/PowerAlertScraper.py
@@ -1,7 +1,7 @@
 """
 PowerAlertScraper: Connect to www.poweralert.co.za, and collect data to use for alerts and analysis.
 """
-import DataAnalysis.WebScaper
+import DataAnalysis.WebScraper
 import re
 
 
@@ -13,25 +13,28 @@ class PowerAlertScraper():
         self.__alert_string = None
         self.__usage = None
         self.__usage_string = None
-        self.__scraper = DataAnalysis.WebScaper.Scraper()
+        self.__scraper = DataAnalysis.WebScraper.Scraper()
         self.__scraper.open_page('http://www.poweralert.co.za/poweralert5/index.php')
 
-    def renew_alert_tag(self):
-        """Retrieves the tag for the current bar in the electricity usage graph"""
+    def renew_tags(self):
+        """
+        Renew the alert tag and usage tags at the same time to reduce network usage
+        """
         bs = self.__scraper.get_beautifulsoup()
         self.__alert = bs.find(lambda tag: tag.name == 'img' and tag.has_attr('width') and tag.has_attr('height') and
                                            tag.has_attr('alt') and tag.has_attr('src') and tag['alt'] == "1")
         if self.__alert is not None:  # Remove <> so re doesn't complain
             self.__alert_string = self.__remove_angle_brackets(self.__alert)
 
-    def renew_usage_tag(self):
-        """Retrieves the tag for the current electricity usage trend"""
-        bs = self.__scraper.get_beautifulsoup()
         self.__usage = bs.find(lambda tag: tag.name == 'img' and tag.has_attr('width') and tag.has_attr('height') and
                                            tag.has_attr('alt') and tag.has_attr('src') and
                                            tag['alt'] == "Electricity usage")
         if self.__usage is not None:  # Remove <> so re doesn't complain
             self.__usage_string = self.__remove_angle_brackets(self.__usage)
+
+    def print_tags(self):
+        print(self.__alert_string)
+        print(self.__usage_string)
 
     @staticmethod
     def __remove_angle_brackets(tag):
@@ -41,15 +44,21 @@ class PowerAlertScraper():
         """Get the current Eskom power alert/usage level. Usage ranges from 0 to 100."""
         """Still need to figure out what useful information can be extracted from this."""
         pattern = re.compile('(?<=height=")[0-9]?[0-9]')   # Compile regex for the height of the current bar
-        match = re.search(pattern, self.__alert_string)    # Find the regex
-        return match.group()    # Will only return one item...I hope!
+        if self.__alert_string is not None:
+            match = re.search(pattern, self.__alert_string)    # Find the regex
+            return match.end()   # Will only return one item...I hope!
+        else:
+            return None
 
     def get_alert_colour(self):
         """Get the current Eskom alert/usage colour.
         Colours are: green, orange, red, black."""
         pattern = re.compile('(?<=bar_)[a-z]{3,6}')  # Compile regex for the colour of the current alert/usage
-        match = re.search(pattern, self.__alert_string)  # Find the regex
-        return match.group()    # Again, only one match returned hopefully!
+        if self.__alert_string is not None:
+            match = re.search(pattern, self.__alert_string)  # Find the regex
+            return match.group()    # Again, only one match returned hopefully!
+        else:
+            return None
 
     def get_usage_status(self):
         """Get the current Eskom usage trend. The trends are 'up', 'down', or 'stable'"""
@@ -57,5 +66,8 @@ class PowerAlertScraper():
         Probably shouldn't expect too much from them anyway."""
         pattern = re.compile(
             '((?<=red_)|(?<=green_)|(?<=yellow_)|(?<=black_))[a-z]{2,6}')   # This looks better than the old regex
-        match = re.search(pattern, self.__usage_string)
-        return match.group()    # And hopefully only one returned here too!
+        if self.__usage_string is not None:
+            match = re.search(pattern, self.__usage_string)
+            return match.group()    # And hopefully only one returned here too!
+        else:
+            return None
