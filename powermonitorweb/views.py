3af5fef491248d9637462be76a90a3b76578cf76
Kevin Beyers <kevinjamesbeyers@gmail.com>
2014-06-26 18:51:00 +0200

    Changed the data models to fit Django's ORM better. Should hopefully make querying a bit easier.

--- powermonitorweb/views.py
+++ powermonitorweb/views.py
@@ -9,16 +9,18 @@ from powermonitorweb.forms import UserForm
 from powermonitorweb.forms import HouseholdSetupUserForm, HouseholdSetupFoodsForm, HouseholdElectricityForm
 from powermonitorweb.models import Food, ElectricityType
 
+
 def index(request):
     context = RequestContext(request)
     return render_to_response('powermonitorweb/index.html', {}, context)
 
+
 def setup_household(request):
-    '''
+    """
     Setup up the household profile
     REQUIRED:   The database table powermonitorweb_issetup must have data. This should be set up when the system
                 is deployed and should not require intervention from the user.
-    '''
+    """
     context = RequestContext(request)
     food_list = Food.objects.all()
     electricity_type = ElectricityType.objects.all()
@@ -32,7 +34,7 @@ def setup_household(request):
         setup_homeowner_form = HouseholdSetupUserForm(data=request.POST)
 
         foods = request.POST.getlist('food_select')
-        elec = request.POST['electricity_select']
+        elec = request.POST['electricity_type']
 
         if setup_homeowner_form.is_valid:
             homeowner = setup_homeowner_form.save()
@@ -47,21 +49,22 @@ def setup_household(request):
     else:
         setup_homeowner_form = HouseholdSetupUserForm()
 
-    print setup
     return render_to_response(
         'powermonitorweb/setup_household.html',
         {'setup_homeowner_form': setup_homeowner_form,
          'food_list': food_list,
-         'electricity_type': electricity_type},
+         'electricity_type': electricity_type,
+         'setup': setup},
         context
     )
 
+
 @login_required()
 def add_user(request):
-    '''
+    """
     Add User view.
     Add a new user to the system. Only the homeowner is able to access this.
-    '''
+    """
     context = RequestContext(request)
     if not request.user.is_authenticated() or not request.user.is_superuser:
         return render_to_response('powermonitorweb/not_admin.html')
@@ -88,6 +91,7 @@ def add_user(request):
         context
     )
 
+
 def user_login(request):
     context = RequestContext(request)
 
@@ -111,6 +115,7 @@ def user_login(request):
             )
     else:
         return render_to_response('powermonitorweb/login.html', {}, context)
+
 
 @login_required()
 def user_logout(request):
