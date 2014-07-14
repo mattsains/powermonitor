df6a5446e1889f00f0c493827586f509a0c2fa84
bentles <dee.bent.lee@gmail.com>
2014-07-14 14:15:49 +0200

    Manage Reports Now has buttons that make sense (hopefully)

--- powermonitorweb/static/powermonitorweb/scripts/manage_reports.js
+++ powermonitorweb/static/powermonitorweb/scripts/manage_reports.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    hideButtons();
+
     $('select#id_occurrence_type').change(function(event)
     {
         if ($(this).val()==0)
@@ -7,4 +9,24 @@ $(document).ready(function(){
         else
             $('input[type=checkbox]').parent().show();
     }).change();
+
+    $("select#id_report_type").change(
+    function()
+    {
+        var enabled = $("select#id_report_type option:selected").attr("data-enabled");
+        hideButtons();
+        if (enabled)
+        {
+            $("input#disable").show();
+            $("input#save").show();
+        }
+        else
+            $("input#enable").show();
+    }
+    );
+
+    function hideButtons()
+    {
+       $("input[type=button]").hide();
+    }
 });
