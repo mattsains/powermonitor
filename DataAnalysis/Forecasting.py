234e36886c13600498701265f57aaecebcdca8db
Kevin Beyers <kevinjamesbeyers@gmail.com>
2014-07-20 23:01:32 +0200

    - Eh. It seems to be graphing predictions correctly, but every now and then it graphs some serious strangeness. Meh, I'm too tired now and should probably get some sleep before varsity.

--- DataAnalysis/Forecasting.py
+++ DataAnalysis/Forecasting.py
@@ -19,7 +19,7 @@ class PowerForecasting:
         # Currently the forecast requires 12 hours of readings with no NaN or inf values
         # So far it seems reasonably accurate to predict the usage for the next hour, but we can play with this
         end = to_datetime(datetime_as_string(resampled_frame.index.values[len(resampled_frame)-1]))  # end of frame
-        start_pred = str(end - relativedelta(hours=12))  # we can play around with this number a bit
+        start_pred = str(end - relativedelta(hours=5))  # we can play around with this number a bit
         end_pred = str(end + relativedelta(hours=1))  # you can try increasing this if you want to
         return result.predict(start=start_pred, end=end_pred)
 
@@ -33,6 +33,8 @@ class PowerForecasting:
         :return: Pandas DataFrame containing predicted values
         """
         if not smooth:
-            return self._ar_prediction(data_frame, '5T')
+            prediction_frame = self._ar_prediction(data_frame, '5T')
+            return prediction_frame[len(prediction_frame)/1.5:]
         else:
-            return ewma(self._ar_prediction(data_frame.reading, '5T'), com=2)
+            prediction_frame = ewma(self._ar_prediction(data_frame.reading, '5T'), com=2)
+            return prediction_frame[len(prediction_frame)/1.5:]
--- powermonitorweb/views.py
+++ powermonitorweb/views.py
@@ -471,15 +471,19 @@ def graphs(request):
                     prediction_frame = pf().predict_usage(data_frame=pre_predction_frame, smooth=True)
                     graph_name = 'prediction_graph.svg'
                     plt().plot_single_frame(data_frame=prediction_frame, title='Predicted Usage', y_label='Usage (kW)',
-                                            x_label='Time', file_name=filename + graph_name)
+                                            x_label='Time', file_name=filename + graph_name, prediction=True)
+                    graph_data=(graph_name, prediction_frame)
             except:
                 graph_data = 'null'
         if graph_data != 'null':
             # A graph was produced!
             # It seemed easier to generate the html tag here, then just use JS to plonk it in the div
-            graph_name = graph_data[0]
-            current_usage = graph_data[1].tail(1).iloc[0]['reading']
-            average_usage = graph_data[1].mean(axis=0)['reading']
+            try:
+                graph_name = graph_data[0]
+                current_usage = graph_data[1].tail(1).iloc[0]['reading']
+                average_usage = graph_data[1].mean(axis=0)['reading']
+            except:
+                graph_name = graph_data[0]
             eskom_status = scraper.instance().get_alert_colour()
             graph_html = "<img src='/static/powermonitorweb/images/graphs/%s' />" % graph_name
         else:
@@ -495,7 +499,7 @@ def graphs(request):
                 pass
             graph_html = "<strong>No graph found. There may be insufficient data to generate a graph.<br />" \
                          "Try selecting another period.</strong>" \
-                         "<p>If a power outage has occurred, you may need to wait 12hours before predictions can be" \
+                         "<p>If a power outage has occurred, you may need to wait 12hours before predictions can be " \
                          "calculated</p>"
         JSONdata = '{"graph": "%s"}' % graph_html  # return the name of the new graph to display
         return HttpResponse(JSONdata)
@@ -508,7 +512,6 @@ def graphs(request):
         eskom_colour = scraper.instance().get_alert_colour()
         eskom_status = scraper.instance().get_usage_status()
         status_image = '%s_%s.svg' % (eskom_colour, eskom_status)
-
 
     return render_to_response(
         'powermonitorweb/graphs.html',
