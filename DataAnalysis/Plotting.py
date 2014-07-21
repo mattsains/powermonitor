47726ec37581080b90ea6a186b2af9f705c76a5f
Vincent <hobolicious101@gmail.com>
2014-07-21 16:36:00 +0200

    Some ish

---
+++ DataAnalysis/Plotting.py
@@ -0,0 +1,180 @@
+from statsmodels.discrete.discrete_model import L1BinaryResults
+
+__author__ = 'Vincent'
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from io import StringIO
+class Plotter():
+    """Plotter"""
+
+    def __init__(self):
+        """Stuff that must be initialized when this class is created"""
+
+    def expoweightedresampling(self,dateFrameIn,amountOfWeight = 1,freqVal='1min',min_periodsVal=10):
+        """Specify the dateFrame that you want to plot
+        :rtype : object
+        freqVal: "1min","1H", "1M", "1S" or any multiples there of
+        amountOfWeight: This is how much does the observation affect the exponentially weighted mean
+        min_periodsVal: This is the number of values you need in order for this plot to be useful
+
+        NOTE:
+        This emwa is a form a resampling, so you could actually use this to resample data.
+        The great thing is that it places extra weight on observations with a recent time stamp
+        The 1 minute frequency is the accepted min for frequency, even though if this is done over a few weeks
+        would possibly want freq to be a few hours, will refactor for that case. This is v1.0
+
+        Return: This will return a dateFrame resampled under the Exponential weight moving average principal
+        """
+        if (len(dateFrameIn) == 0):
+            raise ValueError('Invalid DateFrame, Please pass DateFrame with actually data')
+        if (amountOfWeight<= 0):
+            raise ValueError("The amount of weight is incorrect, should be a float > 0")
+        if (min_periodsVal<= 0):
+            raise ValueError("The min number of periods isn't valid")
+        toReturn = None
+        try:
+            toReturn = pd.ewma(dateFrameIn,amountOfWeight,freq=freqVal,min_periods=min_periodsVal)
+        except:
+            raise ValueError("The frequency %d isn't valid for Pandas",freqVal)
+        return toReturn
+
+        # still need to decide if maybe i should refactor this into two functions
+        # one that returns the fig as a file from the plot
+        # one that down samples the data using EWMA
+
+    def plotDataBetweenTwoDateFrames(self,dateFrameOriginal,dateFrameWeighted,LegendLabelOriginal="Current reading",
+                                     LegendLabelWeighted="weighted plot",Title="YourTitle",YLabel="Y",XLabel="X",
+                                     fileName="plotted.svg"):
+        """Specify the dateFrame that you want to plot, a weighted vs actual readings currently
+        Title: The title for the plot : "THE BEST PLOT EVER"
+        YLabel: The label for the y axis : reading(kWh)
+        XLabel: The label for the x axis : Time Stamp
+        fileName: file name you want to save as
+
+        Return: return the figure or a stringIO of the figure
+        """
+        FileToReturn = None
+        canSave = False
+        splitted = fileName.split(".")
+        buffer = StringIO()
+        if (len(LegendLabelOriginal) <= 0):
+                LegendLabelOriginal="current readings"
+        if (len(LegendLabelWeighted) <= 0):
+                LegendLabelWeighted="weighted readings"
+
+        if ((splitted[1] == "svg") or (splitted[1] == "png") or (splitted[1] == "jpg") ):
+            canSave = True # will add more
+            #fig = plt.figure() # not needed # the below needs to still be tested, just going on logic
+            dateFrameOriginal.reading.plot(label =LegendLabelOriginal,color='g')
+            dateFrameWeighted.plot(label = LegendLabelWeighted,color ='Y',marker ='o')
+
+            if ((len(Title) > 0) and (len(YLabel)>0) and (len(XLabel) > 0)):
+                plt.legend()
+                plt.ylabel(YLabel)
+                plt.xlabel(XLabel)
+                plt.title(Title)
+                FileToReturn = plt.savefig(fileName) #1
+                # or
+                plt.savefig(buffer) #2
+        # Apparently this is a good alternative, the above is just a logical flow
+
+
+
+        plot_data = buffer.getvalue()
+        return FileToReturn
+        # thus could either return file to return or
+        return plot_data
+
+
+
+    def turnDateFrameIntoGraph(self,dateFrameIn,amountOfWeight = 1,freqVal='1min',min_periodsVal=10,
+                               Title="YourTitle",YLabel="Y",XLabel="X",fileName="plotted.svg",typeWeight = "EWMA"):
+        """Specify the dateFrame that you want to plotted against its EMWA
+        you require the parameters to shape the plot.
+        The parameters are explained in the self.expoweightedresampling and self.plotDataBetweenTwoDateFrames
+        fileName: This will be what you want to save the file as < not sure if I should keep this
+        or hard code the fileName to keep consistency
+        Return: This will attempt to return the file that stores the figured for the plot
+        """
+        dateFrameWeighted = None
+        LegendToSend =""
+        ## check length of typeWeight
+        if (typeWeight == "EWMA"):
+            dateFrameWeighted = self.expoweightedresampling(dateFrameIn,amountOfWeight,freqVal,min_periodsVal)
+            LegendToSend= "Exponential weighted moving average plot"
+        else:
+            dateFrameWeighted = self.equalWeightMovingAverage(dateFrameIn,freqVal,min_periodsVal)
+            LegendToSend= "Equal weighted moving average plot"
+
+        return self.plotDataBetweenTwoDateFrames(dateFrameIn,dateFrameWeighted,LegendLabelWeighted=LegendToSend,
+                                                 Title=Title,YLabel=YLabel,XLabel=XLabel,FileName =fileName)
+
+    def equalWeightMovingAverage(self,dateFrameIn,freqVal='1min',min_periodsVal=10):
+        """Specify the dateFrame that you want to plot
+        :rtype : object
+        freqVal: "1min","1H", "1M", "1S" or any multiples there of
+        min_periodsVal: This is the number of values you need in order for this plot to be useful
+        NOTE:
+        The 1 minute frequency is the accepted min for frequency, even though if this is done over a few weeks
+        would possibly want freq to be a few hours, will refactor for that case. This is v1.0
+
+        Return: This will return a dateFrame resampled under the equal weight moving average principal
+        """
+
+        if (dateFrameIn == None):
+            raise ValueError('Invalid DateFrame, Please pass DateFrame with actually data')
+        if (min_periodsVal<= 0):
+            raise ValueError("The min number of periods isn't valid")
+        toReturn = None
+        try:
+            toReturn = pd.rolling_mean(dateFrameIn.reading,min_periodsVal,freqVal)
+        except:
+            raise ValueError("The frequency %d isn't valid for Pandas",freqVal)
+        return toReturn
+
+    def singlePlotOfFrame(self,dateFrameIn,Title="YourTitle",lengendLabel="plot 1",YLabel="Y",XLabel="X",
+                                     fileName="plotted.svg"):
+        """Specify the dateFrame that you want to plot, single dateFrame
+        Title: The title for the plot : "THE BEST PLOT EVER"
+        YLabel: The label for the y axis : reading(kWh)
+        XLabel: The label for the x axis : Time Stamp
+        fileName: file name you want to save as
+
+        Return: return the figure or a stringIO of the figure
+        """
+        FileToReturn = None
+        canSave = False
+        splitted = fileName.split(".")
+        buffer = StringIO()
+        if ((splitted[1] == "svg") or (splitted[1] == "png") or (splitted[1] == "jpg") ):
+            canSave = True # will add more
+            #fig = plt.figure() # not needed # the below needs to still be tested, just going on logic
+            if (len(lengendLabel) <= 0):
+                lengendLabel="plot 1"
+
+            dateFrameIn.reading.plot(label=lengendLabel,color='g') # should probably check if the plotting actually needs reading or not
+            if (len(Title) <= 0):
+                Title = "A plot"
+            if ((len(YLabel)<=0)):
+                YLabel = "Y"
+            if (len(XLabel) <= 0):
+                XLabel="X"
+
+
+            plt.legend()
+            plt.ylabel(YLabel)
+            plt.xlabel(XLabel)
+            plt.title(Title)
+            FileToReturn = plt.savefig()
+            # or
+            plt.savefig(buffer)
+
+        # Apparently this is a good alternative, the above is just a logical flow
+
+
+
+        plot_data = buffer.getvalue()
+        return FileToReturn
+        # thus could either return file to return or
+        return plot_data
---
+++ DataAnalysis/Resampling.py
@@ -0,0 +1,169 @@
+__author__ = 'Vincent'
+# This class will resample the data depending on different needs
+import pandas as pd
+import numpy as np
+class Resampling():
+    """Resampler."""
+
+    def __init__(self):
+        """Stuff that must be initialized when this class is created"""
+
+
+    def downsampleDateFrame(self,dataFrameIn,freq="1min",closedVal = "left",labelVal="left",kindVal ="timestamp",howVal = None):
+        """Specify the dateFrame that you want to resample to allow graphs to be not overfilled.
+        freq: "1min","1H", "1M", "1S" or any multiples there of
+        closed: this shows which side to include in the down sampling. ie, "left", "right"
+        label: this is to show how the time stamps will be labelled from. ie, "left", "right"
+        kind: Either timestamp or period depending on your need, generally time stamp works better
+
+        Example: downsample(df,freq="5min",closed ="left",label= "left",how ="mean")
+        This will then resample the dataframe df into 5 minute intervals and the intervals will be
+        recalculated using the mean of the interval. The left value of the interval is excluded
+        the timestamp will start on the left side of the interval.
+
+        #df2.resample('1min',how='mean',axis = 0,closed ='right',label = 'left',kind ='timestamp')
+
+        Returns a pandas DataFrame object with name which is the timestamp, it will have
+        a reading column, this DataFrame will be resampled the way you need it to be"""
+
+        #this is the resample method
+        # Ask kevin about checking type to dataFrame
+        # if (type(dateFrameIn) == pd.DateFrame):
+
+        if (dataFrameIn.__len__() == 0):
+            raise ValueError('Invalid DateFrame, Please pass DateFrame with actually data')
+        if (not(closedVal.__eq__("left")) or (closedVal.__eq__("right"))):
+            raise ValueError("The closed side is incorrect, either it's left or right")
+        if (not(labelVal.__eq__("left")) or (labelVal.__eq__("right"))):
+            raise ValueError("The label is incorrect, either it's left or right")
+        if (not(kindVal.__eq__("timestamp")) or (kindVal.__eq__("period"))):
+            if (not(kindVal == None)):
+                raise ValueError("The kind of DateFrame is incorrect, please input the correct type")
+
+        toReturn = None
+        try:
+            toReturn = dataFrameIn.resample(freq,how= howVal,closed= closedVal,label= labelVal,kind= kindVal)
+        except:
+            raise ValueError("The frequency inputted isn't one that Pandas can identify, please input correct frequency")
+
+        return toReturn
+
+        #dateFrameIn.resample('1min',how='mean',axis = 0,closed ='right',label = 'left',kind ='timestamp')
+    def getmaxminforgraphing(self,dateFrameIn,column='reading'):
+        """
+        This will take a Pandas DateFrame
+        column: You can choose what column you want to find max and min value
+        max, min: just so you can get multiple values from this method back
+        :return: no specific return, this will be done via parameters max, min
+        """
+
+        if (dateFrameIn is None):
+            raise ValueError('Invalid DateFrame, Please pass DateFrame with actually data')
+        try:
+            columnArray = dateFrameIn.ix[:,column] #check column
+            #print(maxCollumn)
+            max = 0
+            if (len(columnArray) == 0):
+                raise ValueError("There isn't any data to calculate a min and max")
+
+            for x in range(0, len(columnArray)):
+                if (max < columnArray[x]):
+                    max = columnArray[x]
+
+            min = max
+            for x in range(0, len(columnArray)):
+                if (min > columnArray[x]):
+                    min = columnArray[x]
+            return (min, max)  # return the min and max values
+            ## return int array
+        except:
+            raise ValueError("The column %s specified doesn't exist in this Pandas DateFrame",column)
+
+    def getmoreaccuratestd(self,dateFrameIn,column="reading"):
+        """
+        This will take a Pandas DateFrame
+        :return: This will return a Standard Deviation of the Pandas DateFrame with higher accuracy
+        """
+        if (dateFrameIn == None):
+            raise ValueError('Invalid DateFrame, Please pass DateFrame with actually data')
+        columnArray = None
+        try:
+            columnArray = dateFrameIn.ix[:,column]
+        except:
+            raise ValueError("The column %s doesn't exist in the provided DateFrame",column)
+
+        npaColumnArray = np.array(columnArray)
+        stdworked = npaColumnArray.std()
+        if (stdworked == 0):
+            raise ValueError("Please input a DateFrame with actual data in")
+
+        return stdworked
+
+    def getDateFrameStd(self,dateFrameIn,column="reading"):
+        """
+        This will take a Pandas DateFrame this dataFrame should be resampled
+        :return: This will return a Standard Deviation of the Pandas DateFrame
+        """
+        if not dateFrameIn:
+            raise ValueError('Invalid DateFrame, Please pass DateFrame with actually data')
+        stdworked = 0
+        if (len(column) <= 0):
+            raise ValueError("Invalid column value to find std on")
+
+        try:
+            stdworked = dateFrameIn.std(column)
+        except:
+            raise ValueError("The column %s doesn't exist in the provided DateFrame",column)
+
+        if (stdworked == 0):
+            raise ValueError("Please input a DateFrame with actual data in")
+
+        return stdworked
+
+    def getListOfOutliers(self,dateFrameIn,freq="1min",closedVal = "left",labelVal="left",kindVal ="timestamp",column="reading"):
+        """
+        This will take a Pandas DateFrame this dataFrame should be resampled
+        freq,closedVal,LabelVal,kindVal,howVal: are for the resampling and will be handled by the method
+        downsampleDateFrame, this will raise a valueError
+        column: This is the column you want to see outliers on, generally will be the "reading" column
+        Reason for duplicating the default values is for less error checking and can call the downSampleDataFrame method
+        without any concern
+        :return: This will return list of DateTime that will be all the outliers
+        """
+        if not dateFrameIn:
+            raise ValueError('Invalid DateFrame, Please pass DateFrame with actually data')
+        DateFrameMean = None
+        try:
+            DateFrameMean = self.downsampleDateFrame(dateFrameIn,freq,closedVal,labelVal,kindVal,howVal="mean")
+        except:
+            raise ValueError("One of the parameters is incorrect")
+        DateFrameOriginal = None
+
+        try:
+            DateFrameOriginal = self.downsampleDateFrame(dateFrameIn,freq,closedVal,labelVal,kindVal)
+        except:
+            raise ValueError("One of the parameters is incorrect")
+
+        stdworked = 0
+
+        try:
+            stdworked = self.getmoreaccuratestd(dateFrameIn,column)
+        except:
+            raise ValueError("The column doesn't exist")
+
+        toBeOutlier = 3*stdworked
+
+        # Two arrays to get the outliers, the arrays allow us to compare values directly
+        # No try is used because if it failed before on column, would fail again
+
+        ArrOfMean = DateFrameMean.ix[:,column]
+        arrTrueValue = DateFrameOriginal.ix[:,column]
+        dateTimeList = []
+
+        for x in range(0, len(ArrOfMean)-1):
+            difference = arrTrueValue[x] - ArrOfMean[x]
+            if (difference >= toBeOutlier):
+                dateTimeList.append(str(dateFrameIn.ix[x]))
+
+        return dateTimeList
+
