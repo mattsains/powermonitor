5cce1bafd1a9ec6b86a58d48ee1c4ea0a88616ef
bentles <dee.bent.lee@gmail.com>
2014-07-31 22:45:59 +0200

    Fixing the fixes

--- powermonitorweb/static/powermonitorweb/scripts/manage_reports.js
+++ powermonitorweb/static/powermonitorweb/scripts/manage_reports.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
     var messageAsAlert = ecoberry.ajax.messageAsAlert;
     
     /*Change the details in the fields to match the selected entry */
-    $("#id_report_type").change(
+    var $reports = $("#id_report_type"); 
+    
+    $reports.change(
 	createPOSTFunction("/powermonitorweb/manage_reports/", "#id_report_type", "id_report_type_change",
 			   createFieldFiller("occurrence_type", "datetime", "report_daily", "report_weekly", "report_monthly")));
 
@@ -14,10 +16,12 @@ $(document).ready(function(){
 	createPOSTFunction("/powermonitorweb/manage_reports/", "#manage_reports_form", "enable_report_click",
 			  function(response) { if (messageAsAlert(response))
 					       {
-						   var $reports = $("#id_report_type");
 						   var $newlyenabled = $reports.find(":selected").detach();
-						   $newlyenabled.attr("data-enabled", "true");
-						   $newlyenabled.insertAfter($("#id_report_type option[data-enabled=true]").last())
+						   $newlyenabled.attr("data-enabled", "true");						
+						   if ($("#id_report_type option[data-enabled=true]").length)
+						       $newlyenabled.insertAfter($("#id_report_type option[data-enabled=true]").last());
+						   else
+						       $newlyenabled.insertBefore($("#id_report_type option").first());						   
 						   $reports.change();
 					       }}));
 
@@ -31,10 +35,12 @@ $(document).ready(function(){
 	createPOSTFunction("/powermonitorweb/manage_reports/", "#id_report_type", "disable_report_click",
 			   function(response) {if (messageAsAlert(response))
 			      {
-				  var $reports = $("#id_report_type");
-				  var $newlyenabled = $reports.find(":selected").detach();
-				  $newlyenabled.removeAttr("data-enabled");
-				  $newlyenabled.insertAfter($("#id_report_type option[data-enabled=true]").last())
+				  var $newlydisabled = $reports.find(":selected").detach();
+				  $newlydisabled.removeAttr("data-enabled");
+				  if ($("#id_report_type option[data-enabled=true]").length)
+				      $newlydisabled.insertAfter($("#id_report_type option[data-enabled=true]").last());
+				  else
+				      $newlydisabled.insertBefore($("#id_report_type option").first());						   
 				  $reports.change();
 			      }}));
     
@@ -49,16 +55,19 @@ $(document).ready(function(){
             $('input[type=checkbox]').parent().show();
     }).change();
 
-    $("select#id_report_type").change(
+    $reports.change(
     function () {
         var enabled = $("select#id_report_type option:selected").attr("data-enabled");
+	var $display = $("#display"); 
         hideButtons();
         if (enabled)
         {
+	    $display.css("background-color", "rgb(206, 241, 206)");/*TODO: edit heading and border color? */	    
             $("input#disable_report").show();
             $("input#save_report").show();
         }
         else
+	    $display.css("background-color","white"); /*TODO: edit heading and border color? */
             $("input#enable_report").show();
     }).change();
 
