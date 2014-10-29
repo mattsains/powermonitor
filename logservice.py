d635b5881281c1dc8ea230476bb9aa334e624aba
Matthew Sainsbury <matthew@sainsbury.za.net>
2014-10-29 13:49:10 +0200

    Bugfixes and performance improvements to logservice

--- logservice.py
+++ logservice.py
@@ -20,7 +20,7 @@ if list(rowcount)[0][0] > 0:
         database.execute_query('INSERT INTO powermonitorweb_readings(time, reading) VALUES (DATE_ADD((SELECT MAX(time) \
                                 FROM powermonitorweb_readings as new_time), INTERVAL 1 MINUTE),0);')
         last_time=database.execute_query('select max(time) from powermonitorweb_readings;')
-        if list(last_time)[0][0] >= datetime.now():
+        if list(last_time)[0][0] >= datetime.utcnow():
             break;
 
 print("starting to collect data.")
@@ -29,20 +29,25 @@ while True:
     while (response!=False):
         if response["NO_VOLTAGE"]:
             #Let the web interface know about the lack of voltage signal
-            database.execute_non_query("UPDATE powermonitor_configuration SET value=1 WHERE field='no_voltage'")
+            database.execute_non_query("UPDATE powermonitorweb_configuration SET value=1 WHERE field='no_voltage'")
         if response["OVER_CURRENT"]:
             #Let the web interface know about the damaging current condition
-            database.execute_non_query("UPDATE powermonitor_configuration SET value=1 WHERE field='over_current'")
+            database.execute_non_query("UPDATE powermonitorweb_configuration SET value=1 WHERE field='over_current'")
         
-        readings=[]
+        sum=0
+        count=0
         #Save to the database every minute
         stop_time=datetime.now()+relativedelta(seconds=60)
+        print "Collecting readings..."
         while datetime.now() < stop_time:
-            readings.append(pw.read_watts())
-        average=sum(readings)/len(readings)
+            sum+=pw.read_watts()
+            count+=1
+        print "averaging..."
+        average=sum/count
         if (average<0):
             average=0 #clamp invalid values
-        database.execute_non_query("INSERT INTO powermonitorweb_readings(reading) VALUES (%s)",(average,))
+        print "updating db"
+        print database.execute_non_query("INSERT INTO powermonitorweb_readings(reading) VALUES (%s)",(average,))
     #Something killed the sensor
     #Not sure what to do with this
     #depends if it's transient or chronic
